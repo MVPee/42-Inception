32d83e5827201c257ef8e598adaedc2b4f7e47f2
mvpee <mariusvanpee@hotmail.be>
2024-07-27 21:48:05 +0200

    now work with more space

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# Démarrer MariaDB
 service mysql start
 
-# Attendre que le service soit complètement lancé
-sleep 10
+mysql -u root -e "CREATE DATABASE ${DB_NAME};"
+mysql -u root -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
 
-# Exécuter les commandes SQL
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-
-# Arrêter le serveur MariaDB
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-
-# Démarrer MariaDB en mode sécurisé
 exec mysqld_safe
